1644f0796315c3d560f6c699b6e5d6120e1a81b6
ismailtever <ismailtever94@gmail.com>
2025-12-19 20:40:33 +0300

    Deploy from branch için docs klasörü eklendi

--- vite.config.js
+++ vite.config.js
@@ -15,9 +15,9 @@ export default defineConfig({
       name: 'copy-404',
       closeBundle() {
         // Build sonrası index.html'i 404.html'e kopyala
-        const distPath = join(__dirname, 'dist')
-        const indexPath = join(distPath, 'index.html')
-        const four04Path = join(distPath, '404.html')
+        const docsPath = join(__dirname, 'docs')
+        const indexPath = join(docsPath, 'index.html')
+        const four04Path = join(docsPath, '404.html')
         try {
           copyFileSync(indexPath, four04Path)
           console.log('✓ Copied index.html to 404.html')
@@ -29,7 +29,7 @@ export default defineConfig({
   ],
   base: '/',
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
     sourcemap: false,
   },
 })
