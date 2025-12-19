d0ff466b367c5bbe7ccf0007805078d72013ad2d
ismailtever <ismailtever94@gmail.com>
2025-12-19 19:05:00 +0300

    BrowserRouter HashRouter'a çevrildi - GitHub Pages routing sorunu düzeltildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Privacy from './pages/Privacy';
 import Support from './pages/Support';
